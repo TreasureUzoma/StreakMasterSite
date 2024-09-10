3f2f02110281418cec5564fa3f6e2d2b22e4723a
Treasure Uzoma <treasureuzoma650@gmail.com>
2024-09-10 19:51:21 +0100

    home page done

--- src/components/appcomponents/Footer.jsx
+++ src/components/appcomponents/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer className="absolute text-white bottom-0 left-0 right-0 text-center flex px-7 py-4 justify-center">
+        <footer className="absolute text-white left-0 right-0 text-center flex px-7 py-4 justify-center">
             <p className="text-slate-300">
                 &copy; <span>Codebyte Technologies | All rights reserved</span>
             </p>
--- src/components/appcomponents/MainContent.jsx
+++ src/components/appcomponents/MainContent.jsx
@@ -2,9 +2,105 @@ import React from "react";
 
 function MainContent() {
     return (
-        <main className="flex justify-center">
-            <div className="my_fixed_width">
-
+        <main>
+            <div className="flex justify-center">
+                <div className="my_fixed_width grid place-items-center grid-cols-1 gap-[3rem] md:grid-cols-3">
+                    <div className="w-[15rem] h-[15rem] py-4 border border-1 rounded-3xl border-slate-800 flex text-center p-3 px-5 items-center justify-center">
+                        <div>
+                            <h3 className="text-lg font-bold my-2">
+                                Sleek Design
+                            </h3>
+                            <p className="text-sm">
+                                Equipped with sleek design, Streek promises top
+                                notch user experience
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-[15rem] h-[15rem] py-4 border border-1 rounded-3xl border-slate-800 flex text-center p-3 px-5 items-center justify-center">
+                        <div>
+                            <h3 className="text-lg font-bold my-2">
+                                Fast Response
+                            </h3>
+                            <p className="text-sm">
+                                The app gets more interesting as using it gives
+                                little to zero glitches with responsivity
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-[15rem] h-[15rem] py-4 border border-1 rounded-3xl border-slate-800 flex text-center p-3 px-5 items-center justify-center">
+                        <div>
+                            <h3 className="text-lg font-bold my-2">
+                                Daily Boosts
+                            </h3>
+                            <p className="text-sm">
+                                MasterStreek serves boosters to its users daily
+                                based on their streek points and level of
+                                achievement
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-[15rem] h-[15rem] py-4 border border-1 rounded-3xl border-slate-800 flex text-center p-3 px-5 items-center justify-center">
+                        <div>
+                            <h3 className="text-lg font-bold my-2">
+                                User Authentication
+                            </h3>
+                            <p className="text-sm">
+                                Get access to your streeks on different devices,
+                                fast and secured.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-[15rem] h-[15rem] py-4 border border-1 rounded-3xl border-slate-800 flex text-center p-3 px-5 items-center justify-center">
+                        <div>
+                            <h3 className="text-lg font-bold my-2">
+                                Dark Mode
+                            </h3>
+                            <p className="text-sm">
+                                MasterStreek is available in dark mode for more
+                                comfortable display
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-[15rem] h-[15rem] py-4 border border-1 rounded-3xl border-slate-800 flex text-center p-3 px-5 items-center justify-center">
+                        <div>
+                            <h3 className="text-lg font-bold my-2">
+                                Safe & Secured
+                            </h3>
+                            <p className="text-sm">
+                                Our top priority is to keep your data safe and
+                                secured
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center my-[7rem]">
+                <div className="grid place-items-center grid-cols-1 my-8 gap-[0.2rem] md:gap[4rem] md:grid-cols-2">
+                    <div className="w-[15rem] h-[15rem] py-4 flex text-center items-center justify-center">
+                        <div>
+                            <h2 className="text-2xl font-bold my-2">
+                                Get Started Now!
+                            </h2>
+                            <p className="text-base my-2">
+                                Below are the available platforms to use
+                                MasterStreek:
+                            </p>
+                            <div className="flex justify-center items-center">
+                                <a
+                                    href="#"
+                                    className="inline-flex items-center justify-between bg-gray-300 px-5 py-2 rounded-3xl text-[0.78rem] font-bold text-black my-3"
+                                >
+                                    <span>Web App</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-[15rem] h-[15rem] py-4 flex text-center items-center justify-center">
+                        <div>
+                            <img src="https://streek-app.netlify.app/smartmockups_lndefx112.png" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     );
